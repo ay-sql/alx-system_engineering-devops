ed5a45c3aaa33a493dac28c51baadf544fa22dc3
ay-sql <e.sqli.ayoub@gmail.com>
2024-06-30 00:55:23 +0200

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/puppet
 
-# Install an specific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install required packages
+package { ['ruby', 'ruby-augeas', 'ruby-shadow']:
+  ensure => 'installed',
 }
